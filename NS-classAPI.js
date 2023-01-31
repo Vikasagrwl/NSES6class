6719dcd0c5e35afacd99e05299ca2ebd8d019943
Vikasagrwl <105685053+Vikasagrwl@users.noreply.github.com>
2023-01-31 19:38:56 +0530

    ES6 class

---
+++ NS-classAPI.js
@@ -0,0 +1,41 @@
+class API{
+    url;
+    constructor(url){
+        this.url=url;
+        this.method='GET';
+    }
+    url(){
+       return this.url; 
+    }
+    method(){
+        return this.method;
+    }
+
+    #secure(){
+        if(this.url.indexOf("https://")==0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    isSecure(){
+        if(this.#secure()){
+            return 'true';
+        }
+        else{
+            return 'false';
+        }
+    }
+
+    updateUrl(url){
+        this.url=url;
+    }
+}
+
+const s=new API('https://api. com/api/hello');
+console.log(s.method);
+console.log(s.isSecure());
+s.updateUrl('http://api. com/api/hello');
+console.log(s.isSecure())
+console.log(s.secure);
